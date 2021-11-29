c7ee626649676018a4e6383b454564b1f2da4bad
PedroSalduna <pewemes@gmail.com>
2021-11-29 01:24:39 -0300

    commit 11

--- frontend/src/pags/City.js
+++ frontend/src/pags/City.js
@@ -5,6 +5,7 @@ import Cardcities from '../componentes/Cardcities';
 import Footer from '../componentes/Footer';
 import imagen from '../assets/logocall.png';
 import {Link} from 'react-router-dom'
+// import { withRouter } from '../utils/routes/withRouter';
 
 
 class City extends React.Component {
@@ -17,11 +18,12 @@ class City extends React.Component {
     };
     
     componentDidMount() {
+
+        const id = window.location.href.split("/").pop()
         // console.log(props.match.params.id);
         // console.log(window.location.href);
-        const id = window.location.href.split("/").pop()
         // console.log(id);
-        
+
         axios.get(`http://localhost:4000/api/cities/${id}`)
         .then(res =>this.setState({city:res.data.city}))
     }
@@ -37,11 +39,13 @@ class City extends React.Component {
                      <img  src={this.state.city.imagenCiudad} alt={'imagen city'}/>
                         <h2>{this.state.city.nombreCiudad}</h2>
                         <h2>{this.state.city.pais}</h2>
+                    <h3>Sorry, site under mantaineance</h3>
                      </div>
-                    <h1>Sorry, site under mantaineance</h1>
-                    <Link to={'/cities'}>Back to Cities</Link>
+                     <div>
+                        <Link className='backtocities' to={'/cities'}>Back to Cities</Link>
+                    </div>
                 </div>
-                <Footer/>
+                {/* <Footer/> */}
             </>
     )
  }
