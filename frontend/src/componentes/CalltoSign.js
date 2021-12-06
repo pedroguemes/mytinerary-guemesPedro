337d363802814d5d5385c29466e96ca4491fdded
PedroSalduna <pewemes@gmail.com>
2021-12-06 16:55:16 -0300

    commit 42

--- frontend/src/componentes/CalltoSign.js
+++ frontend/src/componentes/CalltoSign.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
+import {Link} from 'react-router-dom'
 
 function CalltoSign() {
   return (
-    <Alert className="cartelsignin" variant="secondary">
-      <Alert.Heading>Sign Up!</Alert.Heading>
-      <p>Join us and post your own itineraries by filling these fields ➡</p>
+    <Alert className="cartelsignup" variant="secondary">
+      <Alert.Heading className="signupcarteltitle">Sign Up!</Alert.Heading>
+      <p>Join us and post your own itineraries by filling these fields</p>
+      <p className="cartelsignupPchico"> ..Already a member?, <Link to="/signin">Sign In here ⬅</Link></p>
     </Alert>
   );
 }
--- frontend/src/pags/SignIn.js
+++ frontend/src/pags/SignIn.js
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
 import Navheader from "../componentes/Navheader";
 import Footer from "../componentes/Footer";
+import CalltoSign from "../componentes/CalltoSign";
+import {Link} from 'react-router-dom'
 
 export default class SignIn extends React.Component {
   render() {
     return (
       <>
-        <Navheader />
-            <div>
+        <Navheader/>
+          <div className="formcontainerIn">
+             {/* <div>
+                  <p>hola</p>
+             </div> */}
+             <div className="formsignin">
+             <div className="containerinternoIn">
+             <div>
+                        <p>Welcome back! :)</p>
+             </div>
+             <div>
                 <form>
-                      <h1>formulario SIGNIN</h1>  
-                </form>
+                        <div>
+                            <input type="text" name="userName" placeholder="Username" />
+                        </div>
+                        <div>
+                            <input type="password" name="password" placeholder="Password"/>
+                        </div>
+                    </form>
+                    <div className="formButtons">
+                        <button >Sign in</button>
+                    </div>
+                    <div>
+                        <p>Not a member?, <Link to="/signup">Sign Up !</Link></p>
+                    </div>
+             </div>
+             </div>
             </div>
+          </div>
         <Footer />
       </>
     );
